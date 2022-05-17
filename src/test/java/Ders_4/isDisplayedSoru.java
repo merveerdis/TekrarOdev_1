b753f6a28062471b604145f912def100027874a6
Merve <merveerdis@outlook.com>
2022-05-17 20:27:56 +0200

    on yedi mayis

--- src/test/java/Ders_4/isDisplayedSoru.java
+++ src/test/java/Ders_4/isDisplayedSoru.java
@@ -1,6 +1,58 @@
 package Ders_4;
-
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+import java.util.concurrent.TimeUnit;
 public class isDisplayedSoru {
-
-
+    static WebDriver driver;
+    @BeforeClass
+    public static void setUp() {
+        WebDriverManager.chromedriver().setup();
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+    }
+    @BeforeMethod
+    public void beforeMethod() {
+        driver.get("https://www.bestbuy.com");
+    }
+    @Test
+    public void titleTest(){
+        String title=driver.getTitle();
+        System.out.println(title);
+        boolean gorunuyormu=title.contains("Best");
+        Assert.assertTrue(gorunuyormu);
+    }
+    @Test
+    public void logo(){
+        WebElement logo=driver.findElement(By.className("logo"));
+        boolean gorunuyormu=logo.isDisplayed();
+        Assert.assertTrue(gorunuyormu);
+    }
+    @Test
+    public void link(){
+        WebElement link=driver.findElement(By.linkText("Mexico"));
+        //  WebElement linkMexico= driver.findElement(By.partialLinkText("Mex"));
+        boolean gorunuyormu=link.isDisplayed();
+        Assert.assertTrue(gorunuyormu);
+    }
+    @AfterClass
+    public void tearDown(){
+        driver.close();
+    }
 }
+
+
+
+
+
+
+
+
